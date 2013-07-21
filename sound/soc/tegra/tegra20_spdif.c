d0a201c6e1a69ca76c9a17998182d557081025cb
Richard Zhao <rizhao@nvidia.com>
2013-07-21 10:34:09 +0800

    ASoC: tegra: correct playback_dma_data setup

--- sound/soc/tegra/tegra20_spdif.c
+++ sound/soc/tegra/tegra20_spdif.c
@@ -351,9 +351,15 @@ static int tegra20_spdif_platform_probe(struct platform_device *pdev)
 	}
 
 	spdif->playback_dma_data.addr = mem->start + TEGRA20_SPDIF_DATA_OUT;
+<<<<<<< HEAD
 	spdif->playback_dma_data.wrap = 4;
 	spdif->playback_dma_data.width = 32;
 	spdif->playback_dma_data.req_sel = dmareq->start;
+=======
+	spdif->playback_dma_data.addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES;
+	spdif->playback_dma_data.maxburst = 4;
+	spdif->playback_dma_data.slave_id = dmareq->start;
+>>>>>>> 647ab784c507... ASoC: tegra: correct playback_dma_data setup
 
 	pm_runtime_enable(&pdev->dev);
 	if (!pm_runtime_enabled(&pdev->dev)) {
