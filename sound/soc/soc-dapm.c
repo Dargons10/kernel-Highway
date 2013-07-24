c531d1d47ebf9dccdd975f4d971de061c43bf683
Lars-Peter Clausen <lars@metafoo.de>
2013-07-24 15:27:35 +0200

    ASoC: dapm: Fix return value of snd_soc_dapm_put_{volsw,enum_virt}()
    The ALSA core expect the put callback of a control to return 1 if the value of
    the control changed and 0 if it did not. Both snd_soc_dapm_put_volsw() and
    snd_soc_dapm_put_enum_virt() currently always returns 0. For both functions we
    already have a 'change' variable which either contains 1 or 0 depending on
    whether the value has changed or not, so just return that.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2739,7 +2739,7 @@ int snd_soc_dapm_put_volsw(struct snd_kcontrol *kcontrol,
 	}
 
 	mutex_unlock(&card->dapm_mutex);
-	return 0;
+	return change;
 }
 EXPORT_SYMBOL_GPL(snd_soc_dapm_put_volsw);
 
@@ -2867,7 +2867,6 @@ int snd_soc_dapm_put_enum_virt(struct snd_kcontrol *kcontrol,
 	struct soc_enum *e =
 		(struct soc_enum *)kcontrol->private_value;
 	int change;
-	int ret = 0;
 	int wi;
 
 	if (ucontrol->value.enumerated.item[0] >= e->max)
@@ -2887,7 +2886,7 @@ int snd_soc_dapm_put_enum_virt(struct snd_kcontrol *kcontrol,
 	}
 
 	mutex_unlock(&card->dapm_mutex);
-	return ret;
+	return change;
 }
 EXPORT_SYMBOL_GPL(snd_soc_dapm_put_enum_virt);
 
